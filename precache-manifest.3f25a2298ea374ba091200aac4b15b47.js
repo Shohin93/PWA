2f78cca691f26fe294cc083a97d4dba885567cc7
Shohin Suhrabi <shohin.suhrabi@customink.com>
2020-04-03 11:36:29 -0400

    Updates

---
+++ precache-manifest.3f25a2298ea374ba091200aac4b15b47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58ac22bd536cc460c56746691543d431",
+    "url": "/PWA/index.html"
+  },
+  {
+    "revision": "79f11b1f9733ea7efb45",
+    "url": "/PWA/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1d5bb00e9dd31aaa5945",
+    "url": "/PWA/static/js/2.85c755b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PWA/static/js/2.85c755b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79f11b1f9733ea7efb45",
+    "url": "/PWA/static/js/main.a9c6ca03.chunk.js"
+  },
+  {
+    "revision": "5e599d3e3c197ec0cc4c",
+    "url": "/PWA/static/js/runtime-main.2296aab9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PWA/static/media/logo.5d5d9eef.svg"
+  }
+]);
